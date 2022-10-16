0aa20e00e2e5d82caaa2268fadd10c28ea0b7fdd
Mateusz Ryczek <mateusz@u.northwestern.edu>
2022-10-16 15:11:44 -0500

    Update fake posts

--- src/server/seeders/20221009195440-fake-posts.js
+++ src/server/seeders/20221009195440-fake-posts.js
@@ -7,7 +7,7 @@ module.exports = {
     ).then((users) => {
       const usersRows = users[0];
       return queryInterface.bulkInsert('Posts', [{
-        text: 'Lorem ipsum 1',
+        text: 'Test post 1',
         userId: usersRows[0].id,
         createdAt: new Date(),
         updatedAt: new Date(),
