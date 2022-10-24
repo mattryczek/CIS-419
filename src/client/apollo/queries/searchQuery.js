faac258f95c85a252b36731ea08d32d3635051cd
Mateusz Ryczek <mateusz@u.northwestern.edu>
2022-10-23 23:17:23 -0500

    Fix user search query

--- src/client/apollo/queries/searchQuery.js
+++ src/client/apollo/queries/searchQuery.js
@@ -6,7 +6,6 @@ export const GET_USERS = gql`
   query usersSearch($page: Int, $limit: Int, $text: String!) {
     usersSearch(page: $page, limit: $limit, text: $text) {
       users {
-        id
         ...userAttributes
       }
     }
