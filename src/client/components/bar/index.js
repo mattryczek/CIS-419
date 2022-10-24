5b70f9eb8591cd0fd00e51755a6994d3dd34a703
Mateusz Ryczek <mateusz@u.northwestern.edu>
2022-10-23 23:26:53 -0500

    Add currentUser display

--- src/client/components/bar/index.js
+++ src/client/components/bar/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { UserConsumer } from '../context/user';
 import SearchBar from './search';
+import UserBar from './user';
 
 const Bar = () => {
   return (
     <div className="topbar">
       <div className="inner">
-        <SearchBar/>
+        <SearchBar />
+        <UserConsumer>
+          <UserBar />
+        </UserConsumer>
       </div>
     </div>
   );
---
+++ src/client/components/bar/user.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserBar = ({ user }) => {
+  if(!user) return null;
+  return (
+    <div className="user">
+      <img src={user.avatar} />
+      <span>{user.username}</span>
+    </div>
+  );
+}
+
+export default UserBar
