cfdbbf8ace32db812d646e4e2e8800d52532bb16
Mateusz Ryczek <mateusz@u.northwestern.edu>
2022-11-13 17:27:40 -0600

    Make search results clickable

--- src/client/components/bar/searchList.js
+++ src/client/components/bar/searchList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const SearchList = ({ data: { usersSearch: { users } } }) => {
     const [show, setShowList] = useState(false);
@@ -32,12 +33,16 @@ const SearchList = ({ data: { usersSearch: { users } } }) => {
 
     return (
         show &&
+
         <div className="result">
             {users.map((user, i) =>
-                <div key={user.id} className="user">
-                    <img src={user.avatar} />
-                    <span>{user.username}</span>
-                </div>
+                <Link to={'/user/' + user.username}>
+                    <div key={user.id} className="user">
+                        <img src={user.avatar} />
+                        <span>{user.username}</span>
+                    </div>
+                </Link>
+
             )}
         </div>
     )
